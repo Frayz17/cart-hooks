bdd61a62a781acb5f195e2ee9e2d12acf96f6bbe
Andrey Dmitriev <andrydmit@gmail.com>
2019-11-04 23:54:00 +0200

    worked with handler buy in App component

--- src/App/App.js
+++ src/App/App.js
@@ -41,11 +41,28 @@ function App () {
 
   const handleBuyProduct = (id) => () => {
     const _inventoryItems = { ...inventoryItems }
+    const _cartItems = { ...cartItems }
+
     const key = Object.keys(_inventoryItems).filter(key => _inventoryItems[key].id === id)
     const item = { ..._inventoryItems[key] }
 
-    _inventoryItems[key].quantity = _inventoryItems[key].quantity - 1
-    console.log(_inventoryItems)
+    // _inventoryItems[key].quantity = _inventoryItems[key].quantity - 1
+    // console.log(item)
+
+    if (existInCart(_cartItems, id)) {
+      _cartItems[id].quantity += 1
+    } else {
+      _cartItems[id] = (
+        id: {
+          id: item.id,
+          title: item.title,
+          price: item.price,
+          quantity: 0
+        }
+      )
+    }
+
+    console.log(_cartItems)
 
     setCartItems({
       ...cartItems,
@@ -54,6 +71,10 @@ function App () {
     setInventoryItems({
       ..._inventoryItems
     })
+  }
+
+  const existInCart = (obj, prop) => {
+    return Object.prototype.hasOwnProperty.call(obj, prop)
   }
 
   return (
